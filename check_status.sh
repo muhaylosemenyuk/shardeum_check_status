e7c7be76d59c5d5046124b2377f7f2304995bdaf
MUHAILO SEMENIUK <79005788+muhaylosemenyuk@users.noreply.github.com>
2023-03-07 17:31:06 +0200

    Create check_status.sh

---
+++ check_status.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+check_status() {
+ while :
+ do 
+     if operator-cli status | grep "stopped"; then
+         echo "---Trying to restart---"
+         operator-cli start
+     elif operator-cli status | grep "standby"; then
+         echo "Waiting for active status"
+     fi
+ sleep 2
+ done
+
+main() {
+  echo "start main"
+  check_status
+}
+main
